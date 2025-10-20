21b6a247eb28d43e1773a54919b71fc6e22dbd78
Davy <davy@datalink.be>
2025-10-20 14:21:25 +0200

    refactor(routing): add names for ziggy.js compatibility on front-end (needed for 9231af73d863aa778f2e3a60218b159d4cb9cf11)

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,16 @@ use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('Home');
-});
-
-
+})->name('home');
 
 Route::get('/upload', function () {
     return Inertia::render('Upload');
-});
+})->name('upload');
+
+Route::get('/profile', function () {
+    return Inertia::render('Profile');
+})->name('profile');
+
+Route::get('/logout', function () {
+    return Inertia::render('Home');
+})->name('logout');
