33a62523a3f5cc4926b14862bb8a969c649b8a35
Davy <davy@datalink.be>
2025-10-20 13:45:09 +0200

    feat: add home page with inertia compatible pagination component

---
+++ resources/js/components/Pagination.vue
@@ -0,0 +1,42 @@
+<template>
+
+    <div class="flex py-2 justify-center items-center bg-white rounded">
+        <div class="select-none flex flex-wrap items-center justify-center gap-2 text-white">
+            <template
+                v-model="props.links"
+                v-for="(link, index) in links.links"
+                :key="index"
+            >
+                <div
+                    v-if="link.url === null"
+                    class="px-4 py-3 mr-1 text-sm leading-4 text-gray-400 border border-stone-200 bg-stone-100 rounded"
+                >
+                    <span v-html="link.label"></span>
+                </div>
+                <div v-else>
+                    <Link as="button"
+                        class="px-4 py-3 mr-1 text-sm leading-4 rounded  border border-indigo-100 transition duration-300"
+                        :class="link.active == true ? 
+                            'bg-indigo-500 text-white hover:bg-indigo-400' : 
+                            'bg-white text-indigo-700  hover:bg-indigo-50'
+                        "
+                        :href="link.url"
+                        v-html="link.label"
+                    />
+                </div>
+            </template>
+        </div>
+    </div>
+</template>
+
+<script setup>
+	import { Link } from '@inertiajs/vue3';
+    
+	// Important: the prop here must be the full laravel pagination object, not just the links
+	const props = defineProps({
+		links: {
+			type: Object,
+			required: true,
+		},
+	});
+</script>
--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,5 +1,27 @@
 <template>
-   
+    <Layout>
+            <div class="bg-stone-100 min-h-256 p-5">
+                Home content goes here
+            </div>
+
+            <Pagination :links="fakePagination"/>
+    </Layout>
 </template>
 <script setup>
+
+    import Pagination from '@/components/Pagination.vue';
+    import Layout from '@/layouts/Layout.vue';
+
+
+    const fakePagination = {
+        links: [
+            { url: null, label: "&laquo; Previous", active: false }, // disabled previous
+            { url: "/page/1", label: "1", active: false },
+            { url: "/page/2", label: "2", active: true }, // active
+            { url: "/page/3", label: "3", active: false },
+            { url: "/page/4", label: "4", active: false },
+            { url: "/page/3", label: "Next &raquo;", active: false },
+        ],
+    };
+
 </script>
