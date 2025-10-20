b487d7714e87e69687fc4dffc6810eec3ddf8a53
Davy <davy@datalink.be>
2025-10-20 13:48:51 +0200

    feat(home): add imagecard component with icons, update home.vue

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,17 +1,69 @@
 <template>
     <Layout>
-            <div class="bg-stone-100 min-h-256 p-5">
-                Home content goes here
-            </div>
 
-            <Pagination :links="fakePagination"/>
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+            <ImageCard v-for="image in publicImages" :key="image.id" :image="image"/>
+        </div>
+
+        <Pagination :links="fakePagination"/>
     </Layout>
 </template>
 <script setup>
 
-    import Pagination from '@/components/Pagination.vue';
     import Layout from '@/layouts/Layout.vue';
+    import ImageCard from '@/pages/HomePartials/ImageCard.vue';
+    import Pagination from '@/components/Pagination.vue';
 
+    const publicImages = [
+        {
+            id: 1,
+            name: 'sunset.png',
+            size: 54321,
+            url: 'https://via.placeholder.com/256',
+            uploadedAt: '01/01/1970',
+            author: 'John Doe'
+        },
+        {
+            id: 2,
+            name: 'mountains.jpg',
+            size: 123456,
+            url: 'https://via.placeholder.com/256',
+            uploadedAt: '12/12/2022',
+            author: 'Jane Smith'
+        },
+        {
+            id: 3,
+            name: 'sunset.png',
+            size: 54321,
+            url: 'https://via.placeholder.com/256',
+            uploadedAt: '01/01/1970',
+            author: 'John Doe'
+        },
+        {
+            id: 4,
+            name: 'mountains.jpg',
+            size: 123456,
+            url: 'https://via.placeholder.com/256',
+            uploadedAt: '12/12/2022',
+            author: 'Jane Smith'
+        },
+        {
+            id: 5,
+            name: 'sunset.png',
+            size: 54321,
+            url: 'https://via.placeholder.com/256',
+            uploadedAt: '01/01/1970',
+            author: 'John Doe'
+        },
+        {
+            id: 6,
+            name: 'mountains.jpg',
+            size: 123456,
+            url: 'https://via.placeholder.com/256',
+            uploadedAt: '12/12/2022',
+            author: 'Jane Smith'
+        },
+    ];
 
     const fakePagination = {
         links: [
---
+++ resources/js/pages/HomePartials/ImageCard.vue
@@ -0,0 +1,59 @@
+<template>
+    <div
+        class="flex flex-col gap-2 items-center p-4 bg-stone-50 border border-white rounded-lg  hover:shadow-md cursor-pointer hover:scale-105 transition duration-300"
+        @mouseenter="isImageHovered[props.image.id] = true"
+        @mouseleave="isImageHovered[props.image.id] = false"
+        :class="isAnyImageHovered() && !isImageHovered[props.image.id] ? 'grayscale' : 'grayscale-0'"
+    >
+
+        <div class="text-sm w-full flex flex-col gap-2">
+            <div class="flex gap-1  items-center w-full bg-indigo-50 border border-indigo-200 text-indigo-700 justify-between px-2 py-0.5 rounded">
+                <IconPhoto/>
+                <span>{{ props.image.name }}</span>
+            </div>
+
+            <div class="flex gap-1  items-center w-full bg-blue-50 border border-blue-200 text-blue-700 justify-between px-2 py-0.5 rounded">
+                <IconOwner/> 
+                <span>{{ props.image.author }}</span>
+            </div>
+        </div>
+
+        <img
+            :src="props.image.url"
+            alt="Public file"
+            class="w-64 h-64 object-cover rounded border border-indigo-200 bg-indigo-50/50 text-center text-indigo-800"
+        />
+
+        <div class="text-indigo-600 text-sm w-full flex gap-2">
+
+            <div class="flex gap-1  items-center w-full bg-sky-50 border border-sky-200 text-sky-700 justify-between px-2 py-0.5 rounded">
+                <IconDate/>
+                <span>{{ props.image.uploadedAt }}</span>
+            </div>
+            
+
+            <div class="flex gap-1  items-center w-full bg-sky-50 border border-sky-200 text-sky-700 justify-between px-2 py-0.5 rounded">
+                <IconFilesize/>
+                <span>{{ Math.round(props.image.size / 1000) }}kb</span>
+            </div>
+
+        </div>
+    </div>
+</template>
+<script setup>
+    import { reactive } from 'vue';
+    import IconDate from '@/icons/IconDate.vue';
+    import IconFilesize from '@/icons/IconFilesize.vue';
+    import IconOwner from '@/icons/IconOwner.vue';
+    import IconPhoto from '@/icons/IconPhoto.vue';
+
+    const isImageHovered = reactive({});
+    const isAnyImageHovered = () => Object.values(isImageHovered).some(v => v === true);
+
+    const props = defineProps({
+        image: {
+            type: Object,
+            required: true
+        }
+    })
+</script>
