dbd0c19b5f0ecfb84646715231ecfb1085eb585c
Davy <davy@datalink.be>
2025-10-20 13:58:12 +0200

    refactor(searchbar): clean up template, readjust position of clear button and update colspan/grid width

--- resources/js/pages/HomePartials/SearchBar.vue
+++ resources/js/pages/HomePartials/SearchBar.vue
@@ -12,18 +12,26 @@
         </button>
     </div>
 
-    <div v-else-if="showFilterBar" class="grid grid-cols-1 md:grid-cols-4 gap-4 items-center bg-white rounded-lg p-4 border border-stone-200">
-    
-        <div class="w-full">
-            <select id="sort" class="bg-indigo-50 text-sm rounded-md w-full border border-indigo-300 text-indigo-700 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500">
-                <option>Sort by latest photos (public)</option>
-                <option>Sort by lldest photos (public)</option>
-                <option>Sort by largest file size</option>
-                <option>Sort by lmallest file size</option>
-            </select>
-        </div>
+    <div v-else-if="showFilterBar" class="grid grid-cols-1 md:grid-cols-[auto_auto_auto_1fr_auto] gap-4 items-center bg-white rounded-lg p-4 border border-stone-200">
+        <button
+            class="flex gap-6 items-center bg-rose-500 text-white text-sm px-4 py-2 rounded-md hover:bg-rose-600 transition duration-200 cursor-pointer"
+            @click="showFilterBar = false"
+        >
+            <IconFilterRemove/>
+            Reset Filters
+        </button>
 
-        <div class="md:col-span-2 w-full text-indigo-800">
+        <select 
+            id="searchSort" name="searchSort"
+            class="bg-slate-50 text-sm rounded-md w-fit border border-slate-300 text-slate-700 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+        >
+            <option>Sort by latest photos (public)</option>
+            <option>Sort by lldest photos (public)</option>
+            <option>Sort by largest file size</option>
+            <option>Sort by lmallest file size</option>
+        </select>
+
+        <div class="col-span-2 w-full text-indigo-800">
 
             <div class="flex w-full items-center">
                 <select 
@@ -37,7 +45,10 @@
                 </select>
                 <div class="flex border-l-0 w-full border-indigo-200 rounded-r-md border items-center pr-2">
                     <input
-                        id="searchQuery" name="searchQuery" type="text" placeholder="Enter search term..."
+                        id="searchQuery" 
+                        name="searchQuery" 
+                        type="text" 
+                        placeholder="Enter search term..."
                         class="mr-2 w-full px-4 py-2 bg-stoe-50 text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
                     />
 
@@ -45,24 +56,13 @@
                 </div>
             </div>
         </div>
+        <button class="flex items-center w-full gap-6 bg-indigo-500 text-white text-sm px-4 py-2 rounded-md hover:bg-indigo-600 transition duration-200 cursor-pointer">
+            <IconClick/>
+            <span>
+                Search
+            </span>
+        </button>
 
-        <div class="flex items-end justify-end gap-2 h-full">
-            <button
-                class="flex w-full justify-between items-center bg-indigo-500 text-white text-sm px-4 py-2 rounded-md hover:bg-indigo-600 transition duration-200"
-            >
-                <IconClick/>
-                <span>
-                    Search
-                </span>
-            </button>
-            <button
-                class="flex w-full justify-between items-center bg-stone-200/50 text-stone-700 text-sm px-4 py-2 rounded-md hover:bg-stone-300/50 transition duration-200"
-                @click="showFilterBar = false"
-            >
-                <IconFilterRemove/>
-                Clear Filters
-            </button>
-        </div>
     </div>
 </template>
 <script setup>
