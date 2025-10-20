095d8f83029e4573787e34677dbe000f45d18bd9
Davy <davy@datalink.be>
2025-10-20 14:20:49 +0200

    feat: add profile page boilerplate

---
+++ resources/js/pages/Profile.vue
@@ -0,0 +1,19 @@
+<template>
+
+    <Layout tab="Profile">
+
+        <div class="grid grid-cols-[1fr_auto]">
+
+            <div class="">
+                <!-- album space -->
+            </div>
+
+            <div class="">
+                <!-- profile space-->
+            </div>
+        </div> 
+    </Layout>
+</template>
+<script setup lang="js">
+    import Layout from '@/layouts/Layout.vue';
+</script>
