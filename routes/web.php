0d0ab6d85e77e48ab43d2b9cf6cbc69b52de2442
Davy <davy@datalink.be>
2025-10-22 10:56:36 +0200

    fix: route order (standard routes taking priority over resources routes)

--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,15 @@ use Illuminate\Support\Facades\Route;
 
 // images
 Route::get('/', [ImageController::class, 'index'])->name('home');
-Route::get('/image/{image}', [ImageController::class, 'show'])->name('image.show');
 
 // albums
 Route::get('/albums', [AlbumController::class, 'index'])->name('album.index');
-Route::get('/album/{album}', [AlbumController::class, 'show'])->name('album.show');
 
 // resourced resources
 Route::resource('album', AlbumController::class)->middleware('auth')->except(['index', 'show']);
 Route::resource('image', ImageController::class)->middleware('auth')->except(['index', 'show']);
+
+Route::get('/image/{image}', [ImageController::class, 'show'])->name('image.show');
+Route::get('/album/{album}', [AlbumController::class, 'show'])->name('album.show');
 
 require __DIR__.'/auth.php';
