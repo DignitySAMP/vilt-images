d8c1c8612d84aafd8ee34142c5d4ae266a32a9cf
Davy <davy@datalink.be>
2025-10-22 08:45:28 +0200

    feat(images): convert routes to resource

--- routes/web.php
+++ routes/web.php
@@ -4,20 +4,16 @@ use App\Http\Controllers\ImageController;
 use App\Http\Controllers\AlbumController;
 use Illuminate\Support\Facades\Route;
 
+// images
 Route::get('/', [ImageController::class, 'index'])->name('home');
 Route::get('/image/{image}', [ImageController::class, 'show'])->name('image.show');
 
-Route::resource('/album', AlbumController::class)->middleware('auth')->except(['index', 'show']);
-
+// albums
 Route::get('/albums', [AlbumController::class, 'index'])->name('album.index');
 Route::get('/album/{album}', [AlbumController::class, 'show'])->name('album.show');
 
-Route::middleware(['auth'])->group(function () {
-    Route::get('/image/{image}/edit', [ImageController::class, 'edit'])->name('image.edit');
-    Route::patch('/image/{image}', [ImageController::class, 'update'])->name('image.update');
-    Route::delete('/image/{image}', [ImageController::class, 'destroy'])->name('image.destroy');
-    Route::get('/upload', [ImageController::class, 'create'])->name('image.create');
-    Route::post('/upload', [ImageController::class, 'store'])->name('image.store');
-});
+// resourced resources
+Route::resource('album', AlbumController::class)->middleware('auth')->except(['index', 'show']);
+Route::resource('image', ImageController::class)->middleware('auth')->except(['index', 'show']);
 
 require __DIR__.'/auth.php';
